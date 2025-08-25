ab474c93ce7a6a65a0df0709f6e3aff38aee04e4
Tiberiu58 <tiberiumatei1978@gmail.com>
2025-08-25 16:07:26 +0300

    Responsive design + bugfixes

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template
 
+# inițializează aplicația Flask
 app = Flask(__name__)
 
+# ruta principală → pagina de start
 @app.route('/')
 def home():
     return render_template('index.html')
 
+# pornirea aplicației doar local (pe Render se folosește gunicorn, deci if-ul protejează)
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(debug=False, host='0.0.0.0', port=5000)
