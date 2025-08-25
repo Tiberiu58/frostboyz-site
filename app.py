0fa314dad1cfc6df6008e0365ace4188b8d62afb
Tiberiu58 <tiberiumatei1978@gmail.com>
2025-08-25 10:24:29 +0300

    Fix: removed git command from app.py

--- app.py
+++ app.py
@@ -6,13 +6,5 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-# --- NOU: pagina de Instagram ---
-@app.route('/instagram')
-def instagram():
-    return render_template('instagram.html')
-
 if __name__ == '__main__':
     app.run(debug=False)
-
-git config --global user.name "Tiberiu58"
-git config --global user.email "tiberiumatei1978@gmail.com"
